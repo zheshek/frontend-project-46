8eb70402f3561de64c457f10ca2e35f875968139
zheshek <zheny.bulechov@yandex.ru>
2026-01-22 21:34:46 +0300

    Add filepath arguments and format option to CLI

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,4 +5,12 @@ program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format <type>', 'output format')
+  .action((filepath1, filepath2, options) => {
+    console.log(`Comparing ${filepath1} and ${filepath2}`);
+    if (options.format) {
+      console.log(`Format: ${options.format}`);
+    }
+  })
   .parse(process.argv);
--- src/gendiff.js
+++ src/gendiff.js
@@ -4,4 +4,6 @@ program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format <type>', 'output format')
   .parse(process.argv);
