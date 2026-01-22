a436a68ffb6bdd5e4850e84248f78884adee3ec5
zheshek <zheny.bulechov@yandex.ru>
2026-01-22 21:23:06 +0300

    Complete first step: CLI with help and version output

---
+++ src/gendiff.js
@@ -0,0 +1,7 @@
+import { program } from 'commander';
+
+program
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .version('1.0.0')
+  .parse(process.argv);
---
+++ src/index.js
@@ -0,0 +1,3 @@
+export default (filepath1, filepath2) => {
+  return '';
+};
